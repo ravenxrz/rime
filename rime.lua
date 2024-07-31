7e8a5941418f05103f0820d6a45f35267dd8a555
zhangxingrui <zhangxingrui.leo@bytedance.com>
2024-07-31 22:40:46 +0800

    add 虎码

--- rime.lua
+++ rime.lua
@@ -1,17 +1,8 @@
-function date_translator(input, seg)
-   if (input == "orq") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " "))
-   end
-end
-
-function time_translator(input, seg)
-   if (input == "ouj") then
-      local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M"), " ")
-      cand.quality = 1
-      yield(cand)
-   end
-end
-
-calculator_translator = require("calculator_translator")
+charset_comment_filter = require("charset_comment_filter") --Unicode分区提示
+core2022 = require("core2022_filter") --自定义字符集过滤（常用字集）
+-- dz_ci = require("dz_ci_filter") --单字模式 这个别用，有问题的
+number_translator = require("number")
+lua_unicode_display_filter = require("unicode_display")  --Unicode编码提示
+calculator_translator = require("calculator_translator")  --简易计算器
+exe_processor = require("exe")  -- 网页启动器
+shijian2_translator = require("shijian2") -- 高级时间
